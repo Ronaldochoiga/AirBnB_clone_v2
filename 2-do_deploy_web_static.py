1489b31aa8314da320795b118e8e26af2502a193
ronaldochoiga <choigaronaldo@gmail.com>
2023-10-08 06:10:02 -0700

    task 3 correction

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,46 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers both web-01 and web-02
+Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack.
 """
-
-from fabric.api import put, run, env
+from fabric.api import local, put, run
+from datetime import datetime
 from os.path import exists
-env.hosts = ['54.90.59.80', '3.85.54.217']
+
+
+env.hosts = ['3.85.54.217', '54.90.59.80']
+
+
+def do_pack():
+    """
+    Pack webstatic irectory with .tgz extension
+    """
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions")
+    name = "versions/web_static_{}.tgz".format(current_time)
+    fname = local("tar -cvzf {} web_static".format(name))
+    return fname
 
 
 def do_deploy(archive_path):
-    """the script distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
-        return False
+    """
+    Distributes archive to web servers web01 and web02
+    """
+    if exists(archive_path):
+        try:
+            put(archive_path, "/tmp/")
+            fname = archive_path.split("/")[1].split(".")[0]
+            remote_path = "/data/web_static/releases/{}".format(fname)
+            run("mkdir {}".format(remote_path))
+            run("tar -zxvf /tmp/{}.tgz --directory {}/"
+                .format(fname, remote_path))
+            run("rm /tmp/{}".format(archive_path.split("/")[1]))
+            run("rm /data/web_static/current")
+            run("ln -sf /data/web_static/releases/{}\
+                 /data/web_static/current".format(fname))
+            run("mv /data/web_static/releases/{}/web_static/*\
+                 /data/web_static/current/".format(fname))
+            run("rm -rf /data/web_static/releases/{}/web_static/"
+                .format(fname))
+            return True
+    return False
