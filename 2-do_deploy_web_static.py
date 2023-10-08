71af58d38e689d765a25cb301bf0f5da04baef11
ronaldochoiga <choigaronaldo@gmail.com>
2023-10-08 04:46:53 -0700

    task 3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,12 +18,12 @@ def do_deploy(archive_path):
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_exists))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_exists))
+        run('tar -zxvf /tmp/{} -C {}{}/'.format(file_name, path, no_exists))
         run('rm /tmp/{}'.format(file_name))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_exists))
         run('rm -rf {}{}/web_static'.format(path, no_exists))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_exists))
+        run('ln -sf {}{}/ /data/web_static/current'.format(path, no_exists))
         return True
     except:
         return False
