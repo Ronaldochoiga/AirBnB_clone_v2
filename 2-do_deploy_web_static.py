f3736dca23a405dc3d24b2135491f833e3b3a702
ronaldochoiga <choigaronaldo@gmail.com>
2023-10-08 07:53:04 -0700

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers to the specified web servers
-"""
+'''
+Fabric script that distributes an archive to web servers web01 and web02
+'''
 
-from fabric.api import put, run, env
-from os.path import exists
+import os
+from datetime import datetime
+from fabric.api import env, put, run, runs_once, local
+
 env.hosts = ['54.90.59.80', '3.85.54.217']
 
 
-def do_deploy(archive_path):
-    """distributes archive to the web servers 01 and 02"""
-    if exists(archive_path) is False:
-        return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
-        return False
+def do_deploy():
+    """Static files archived by the do_deploy function"""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    time = datetime.now()
+    res = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        time.year,
+        time.month,
+        time.day,
+        time.hour,
+        time.minute,
+        time.second
