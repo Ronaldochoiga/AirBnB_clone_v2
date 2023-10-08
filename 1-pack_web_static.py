e72c37a9bcbe551f8ef921d1d0fc490fe062e3d4
ronaldochoiga <choigaronaldo@gmail.com>
2023-10-08 02:40:30 -0700

    the second task(task 1)

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+this script uses do_pack class.
+the do pack class helps in copressing the files.
+this give an output of .tgz file.
+"""
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+def do_pack():
+    """
+    generates the archive in the tgz format
+    """
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is false:
+            local("mkdir versions")
+        fname = "versions/web_static_{}.tgz".format(date)
+        local("tar =cvzf {} web_static".format(fname))
+        return fname
+    except:
+        return None
