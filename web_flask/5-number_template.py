8cb299390572e9f224f2b46cf7ad61bebbe3f7d2
ronaldochoiga <choigaronaldo@gmail.com>
2023-10-22 14:45:52 -0700

    task 5 code

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application in the command line
+"""
+
+
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    display "Hello HBNB! as the home page"
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    display “HBNB as the about page”
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    display “C ” followed by the value of the text variable provided
+    (replace underscore symbols with a space )
+    """
+    text = text.replace("_", " ")
+    return "C %s" % escape(text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """
+    display “Python ”, followed by the value of the text variable given
+    (replace underscore symbols with a space )
+    """
+    text = text.replace("_", " ")
+    return "Python %s" % escape(text)
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    display “n is a number” Kfor all integers passed only if n is an integer
+    """
+    return "%d is a number" % n
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    display a HTML page when n is an integer
+    """
+    return render_template('5-number.html', number=n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
